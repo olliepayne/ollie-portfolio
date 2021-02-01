0d0c75fc65c2270e51022af7366c546528e0c909
olliepayne <olliepayne@me.com>
2021-02-01 08:39:28 -0700

    add basic slideshow functionality

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,19 +1,39 @@
+import { useState } from 'react'
+import './Slideshow.css'
+
 const imageURLS = [
- 'https://i.imgur.com/AhxonCB.jpg'
+ 'https://i.imgur.com/AhxonCB.jpg', 'https://i.imgur.com/mfE3ILq.jpg'
 ]
 
 const Slide = (props) => {
+ const { imageIndex } = props
+
  return (
-  <img src={props.url}></img>
+  <div className="slide">
+   <img src={imageURLS[imageIndex]}></img>
+   <p>{imageIndex + 1}/{imageURLS.length}</p>
+  </div>
  )
 }
 
 const Slideshow = () => {
+ const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+
+ const previousSlide = () => {
+  const nextIndex = currentSlideIndex - 1 < 0 ? imageURLS.length - 1 : currentSlideIndex - 1
+  setCurrentSlideIndex(nextIndex)
+ }
+
+ const nextSlide = () => {
+  const nextIndex = currentSlideIndex + 1 > imageURLS.length - 1 ? 0 : currentSlideIndex + 1
+  setCurrentSlideIndex(nextIndex)
+ }
+
  return (
   <div>
-   {imageURLS.map((url, index) => (
-    <Slide key={index} url={url} />
-   ))}
+   <Slide imageIndex={currentSlideIndex} />
+   <button onClick={previousSlide}>last</button>
+   <button onClick={nextSlide}>next</button>
   </div>
  )
 }
