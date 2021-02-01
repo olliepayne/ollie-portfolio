bd5823803305678c0014003a3eb3eef8b1f33e02
olliepayne <olliepayne@me.com>
2021-01-31 22:19:20 -0700

    style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
+import './Navbar.css'
+
 const Navbar = () => {
  return (
   <nav>
    <h1>Oliver Payne</h1>
-   <ul>
+   <ul className="nav-links">
     <li><a href="/portfolio">Portfolio</a></li>
     <li><a href="/about">About</a></li>
     <li><a href="/contact">Contact</a></li>
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Landing from '../Landing/Landing'
+import Navbar from '../../components/Navbar/Navbar'
 
 function App() {
  return (
   <div className="App">
-   
+   <Navbar />
   </div>
  );
 }
