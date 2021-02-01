004a32fd90239cb8b92aafac6e8c812bbb5479a2
olliepayne <olliepayne@me.com>
2021-01-31 19:38:49 -0700

    backup

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Landing from '../Landing/Landing'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+ return (
+  <div className="App">
+   
+  </div>
+ );
 }
 
 export default App;
