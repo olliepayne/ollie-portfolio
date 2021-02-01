9c82261cac9681c001eac62a246c09ba8e6c1200
olliepayne <olliepayne@me.com>
2021-01-31 22:09:45 -0700

    stub navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+ return (
+  <nav>
+   <h1>Oliver Payne</h1>
+   <ul>
+    <li><a href="/portfolio">Portfolio</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+   </ul>
+  </nav>
+ )
+}
+
+export default Navbar
