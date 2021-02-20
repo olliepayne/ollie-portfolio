094f59dbb649399f537b9aba34f3859c37e1bfa5
olliepayne <olliepayne@me.com>
2021-02-19 17:41:55 -0700

    update portfolio entry presentation, incorporate new section component

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,6 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import CSS from './Portfolio.module.css'
-
-// add entry logic
-// create personal favorites, and other subsections
 
 const Entry = (props) => {
  const { name, description, links } = props
@@ -13,9 +11,33 @@ const Entry = (props) => {
    <p>{description}</p>
    <ul className={CSS.entryLinks}>
     {links.map((link, index) => (
-     <li><Link to={link.url}>{link.title}</Link></li>
+     <li key={link.url}><a href={link.url}>{link.title}</a></li>
     ))}
    </ul>
+  </div>
+ )
+}
+
+const Section = (props) => {
+ const { name, entries } = props
+ console.log(entries)
+
+ const [isOpen, setIsOpen] = useState(false)
+
+ const handleOpen = () => {
+  setIsOpen(!isOpen)
+ }
+
+ return (
+  <div>
+   <h3 onClick={handleOpen}>{name}</h3>
+   {isOpen &&
+    <>
+     {entries.map((entry) => (
+      <Entry key={entry.name} name={entry.name} description={entry.description} links={entry.links} />
+     ))}
+    </>
+   }
   </div>
  )
 }
@@ -23,10 +45,24 @@ const Entry = (props) => {
 const Portfolio = () => {
  return (
   <div className={CSS.page}>
-   <h3>General Assembly SEI Projects</h3>
-   <Entry name={"Connect 4"} description={"Recreated the classic Connect 4. HTML, CSS, and JavaScript."} links={[{ title: 'Deployment', url: '' }, { title: 'GitHub', url: '' }]} />
-   <Entry name={"Mesocycle Tracker"} description={"Athletic periodization tracking. MEN stack, Google OAuth."} links={[{ title: 'Deployment', url: '' }, { title: 'GitHub', url: '' }]} />
-   <Entry name={"Rosetta Send"} description={"Rock Climbing Route database. MERN stack, custom JWT auth."} links={[{ title: 'Deployment', url: '' }, { title: 'GitHub', url: '' }]} />
+   <Section
+    name={'Favorites'}
+    entries={
+     [
+      { name: 'Rosetta Send', description: 'Rock Climbing Route database. MERN stack, custom JWT auth.', links: [{ title: 'Deployment', url: 'https://blooming-retreat-36750.herokuapp.com/' }, { title: 'GitHub', url: 'https://github.com/olliepayne/rosetta-send-v2' }] }
+     ]
+    }
+   />
+   <Section 
+    name={'General Assembly SEI Projects'}
+    entries={
+     [
+      { name: 'Connect 4', description: 'Recreated the classic Connect 4. HTML, CSS, and JavaScript.', links: [{ title: 'Deployment', url: 'https://opconnect4.surge.sh/' }, { title: 'GitHub', url: 'https://github.com/olliepayne/connect-four' }] },
+      { name: 'Mesocycle Tracker', description: 'Athletic periodization tracking. MEN stack, Google OAuth.', links: [{ title: 'Deployment', url: 'https://quiet-thicket-74088.herokuapp.com/' }, { title: 'GitHub', url: 'https://github.com/olliepayne/unit-2-project/blob/main/README.md' }] },
+      { name: 'Rosetta Send', description: 'Rock Climbing Route database. MERN stack, custom JWT auth.', links: [{ title: 'Deployment', url: 'https://blooming-retreat-36750.herokuapp.com/' }, { title: 'GitHub', url: 'https://github.com/olliepayne/rosetta-send-v2' }] }
+     ]
+    }
+   />
   </div>
  )
 }
