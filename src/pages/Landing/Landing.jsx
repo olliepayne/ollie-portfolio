913abaed294847c25584834425d2d428221ab93e
olliepayne <olliepayne@me.com>
2021-02-21 13:26:44 -0700

    stub about me

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,8 +2,14 @@ import CSS from './Landing.module.css'
 
 const Landing = () => {
   return (
-    <div>
-      
+    <div className={CSS.page}>
+      <h2>About Me</h2>
+      <p>
+        I'm a Fullstack Engineer and Competitive Sport Climber. 
+        Through spending the last two years in the fitness industry, I learned how to achieve goals in the quickest, simplest, and most efficient manner. 
+        My personal ethos is to be confident, yet humble and respectful of all who are around me. 
+        I’m constantly making an effort to grow mentally and physically, which both allow me to efficiently develop my technical skills.
+      </p>
     </div>
   )
 }
