f966346d76a1d306cc5db38111d830162c200fd2
olliepayne <olliepayne@me.com>
2021-02-21 13:44:31 -0700

    backup

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,11 +3,17 @@ import CSS from './Landing.module.css'
 const Landing = () => {
   return (
     <div className={CSS.page}>
+      <div>
+        
+      </div>
       <h2>About Me</h2>
       <p>
-        I'm a Fullstack Engineer and Competitive Sport Climber. 
-        Through spending the last two years in the fitness industry, I learned how to achieve goals in the quickest, simplest, and most efficient manner. 
+        My name is Ollie, and I'm a Fullstack Developer and Competitive Sport Climber!
+        <br />
+        Through spending the last two years in the fitness industry, I learned how to achieve goals in the quickest, simplest, and most efficient manner.
+        <br />
         My personal ethos is to be confident, yet humble and respectful of all who are around me. 
+        <br />
         I’m constantly making an effort to grow mentally and physically, which both allow me to efficiently develop my technical skills.
       </p>
     </div>
