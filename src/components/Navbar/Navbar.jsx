f62e9ab09fe8a3d40adcaf6f77b735cbae1c2109
olliepayne <olliepayne@me.com>
2021-02-21 13:46:04 -0700

    backup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import CSS from './Navbar.module.css'
 const Navbar = () => {
  return (
   <nav className={CSS.nav}>
-   <h1><Link to="/">Oliver Payne</Link></h1>
+   <h1><Link to="/">Ollie Payne</Link></h1>
    <ul className={CSS.navLinks}>
     <li><Link to="/portfolio">Portfolio</Link></li>
     <li><Link to="/contact">Contact</Link></li>
